7892d2174cd441b952bd4c212ad9ffff7b330385
Adrian <adrian.snopek@o2.pl>
2019-05-19 14:45:17 +0200

    Start store

---
+++ vue-learning/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="start">
+
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  computed: {
+    ...mapGetters('store')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
