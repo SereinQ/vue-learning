db198995e7ad8e7f38e4921e6e8b09ed8721e64a
Adrian <adrian.snopek@o2.pl>
2019-05-23 13:21:51 +0200

    Add files

---
+++ vue-learning/src/components/List.vue
@@ -0,0 +1,81 @@
+<template>
+   <div class="start">
+      <div
+         v-for="(car, index) in cars"
+         :key="index"
+         >
+         <span class="d-block">
+          Title: {{car.acf.session[0].title}}
+         </span>
+         <span class="d-block">
+           ID: {{car.id}}
+         </span>
+          <span class="d-block">
+           Slug: {{car.acf.session[0].slug}}
+         </span>
+         <div class="container">
+            <div class="d-inline-block" v-for="(figure, i) in car.acf.session[0].gallery" :key="i">
+              <div class="d-inline-block" v-for="(img, j) in figure.row" :key="j">
+              <router-link :to="{ name: 'single', params: { slug: `${car.id}` } }">
+               <figure class="d-inline-block">
+                  <img class="img" :src="img.src.url" />
+               </figure>
+              </router-link>
+              </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+import { mapGetters, mapState } from 'vuex'
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  computed: {
+    ...mapGetters(['carsMapped']),
+    ...mapState(['cars'])
+  },
+
+  mounted () {
+    this.$store.dispatch('GET_ALL_CARS')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.d-inline-block{
+  display: inline-block;
+}
+
+.d-block{
+  display: block;
+}
+
+.figure-count{
+  width:5px;
+  height: 5px;
+  background-color: black;
+}
+
+.img-count{
+  width:5px;
+  height: 5px;
+  background-color: red;
+}
+
+figure{
+  margin: 10px;
+}
+
+img{
+  max-width: 200px;
+}
+</style>
---
+++ vue-learning/src/components/Single.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="single">
+      <h3>{{currentPost.acf.session[0].title}}</h3>
+      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+         <div class="container">
+            <div class="d-inline-block" v-for="(figure, i) in currentPost.acf.session[0].gallery" :key="i">
+              <div class="d-inline-block" v-for="(img, j) in figure.row" :key="j">
+               <figure class="d-inline-block">
+                  <img class="img" :src="img.src.url" />
+               </figure>
+              </div>
+            </div>
+         </div>
+         <router-link to="/">
+          BACK TO LIST
+          </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    // ...mapGetters(['currentPostsMapped']),
+    ...mapState(['currentPost'])
+  },
+  mounted () {
+    this.$store.dispatch('GET_POST', this.$route.params.slug)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.d-inline-block{
+  display: inline-block;
+}
+
+.d-block{
+  display: block;
+}
+
+.figure-count{
+  width:5px;
+  height: 5px;
+  background-color: black;
+}
+
+.img-count{
+  width:5px;
+  height: 5px;
+  background-color: red;
+}
+
+figure{
+  margin: 10px;
+}
+
+img{
+  max-width: 200px;
+}
+</style>
